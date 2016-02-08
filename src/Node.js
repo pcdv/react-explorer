9a426fa549240c04c38f7664595a877c6103c54c
pcdv <pcdv@local>
2016-02-08 15:32:55 +0100

    play with selection

--- src/Node.js
+++ src/Node.js
@@ -5,17 +5,25 @@ export default class Node extends Component {
   constructor(props) {
     super(props)
     this.toggle = this.toggle.bind(this)
+    this.select = this.select.bind(this)
     this.state = {
-      open: false
+      open: false, selected: false,
     }
   }
 
+  /**
+   * Open or close the node.
+   */
   toggle() {
     if (!this.props.model.isLeaf(this.props.node)) {
       this.setState({
         open: !this.state.open
       })
     }
+  }
+
+  select() {
+    this.setState({selected: !this.state.selected})
   }
 
   render() {
@@ -26,7 +34,8 @@ export default class Node extends Component {
       leaf: model.isLeaf(node),
       open: this.state.open,
       closed: !this.state.open,
-      isDir: !model.isLeaf(node)
+      isDir: !model.isLeaf(node),
+      selected: this.state.selected,
     })
 
     var children
@@ -39,7 +48,7 @@ export default class Node extends Component {
     }
     return (
       <div className={cls}>
-        <div className="overlay"/>
+        <div className="overlay" onClick={this.select}/>
         <div className="handle" onClick={this.toggle}/>
         <div className="label">{model.getLabel(node)}</div>
         {children}
