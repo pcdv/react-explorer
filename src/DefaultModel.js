70ff5fe6717c8333551ecbda2c8168905350c8a5
pcdv <pcdv@nomail>
2016-06-11 14:31:06 +0200

    Doc

--- src/DefaultModel.js
+++ src/DefaultModel.js
@@ -1,3 +1,11 @@
+/**
+ * Default model implementation. Compatible with object trees with the following format:
+ * { label: "foo", children: [
+ *   { label: "bar" }
+ * ]}
+ * 
+ * Property "key" will be used as key if set, otherwise "label" will be used.
+ */
 export default class DefaultModel {
   constructor(root) {
     this.root = root
